990a91ea06f70f9bff103083cc15651ff6056984
JoanAugusto <pauteromeroj@gmail.com>
2024-09-28 20:45:53 +0200

    tercer commit

--- Apuntes JavaScript/Exercise 2/exercise2.js
+++ Apuntes JavaScript/Exercise 2/exercise2.js
@@ -1,30 +1,41 @@
-/*Elabora una secuencia de imagenes con al menos 4 fotogramas.Según el usuario vaya haciendo click sobre las imagenes, la secuencia se irá reproduciendo */
+'use strict';
 
-'use strict'
+const $image = document.getElementById('imagen');
+const $boton = document.getElementById('button');
 
+// Array de URLs de las imágenes
+const imagenes = [
+  'https://th.bing.com/th/id/OIP.CjuJhR9wS4jD9R183-cdDwHaEK?rs=1&pid=ImgDetMain',
+  'https://www.mancity.com/meta/media/jcxpfuu1/gettyimages-1335418526.png',
+  'https://www.20minutos.es/uploads/imagenes/2022/09/07/robert-lewandowski.jpeg',
+  'https://www.prensalibre.com/wp-content/uploads/2019/06/FBL-EUR-NATIONS-POR-NED_45213397.jpg?quality=82' // Agregamos la imagen inicial
+];
 
-const $image=document.getElementById('imagen');
-const $boton=document.getElementById('button');
+// Índice de la imagen actual (comienza en 0)
+let indiceActual = 0;
 
+function manyImages() {
+  // Incrementa el índice actual
+  indiceActual = (indiceActual + 1) % imagenes.length;
 
-
-
-function manyImages(){
-
-   if($image.src.match('green') ){
-    $image.src='https://www.prensalibre.com/wp-content/uploads/2019/06/FBL-EUR-NATIONS-POR-NED_45213397.jpg?quality=82';
-
-
-   }else if($image.src.match('green')){
-    $image.src='https://th.bing.com/th/id/OIP.KvnZQy9Gneq7dTsKaDT12wHaE8?rs=1&pid=ImgDetMain';
-   }else if($image.src.match('green')){
-    $image.src='https://www.mancity.com/meta/media/jcxpfuu1/gettyimages-1335418526.png';
-
-   }else{
-    $image.src='https://www.prensalibre.com/wp-content/uploads/2019/06/FBL-EUR-NATIONS-POR-NED_45213397.jpg?quality=82';
-   }
-    
+  // Usa switch para cambiar la imagen
+  switch (indiceActual) {
+    case 0:
+      $image.src = imagenes[0];
+      break;
+    case 1:
+      $image.src = imagenes[1];
+      break;
+    case 2:
+      $image.src = imagenes[2];
+      break;
+    case 3:
+      $image.src = imagenes[3];
+      break;
+    default:
+      console.log('ERROR: Índice fuera de rango');
+  }
 }
 
-$image.addEventListener('click',manyImages)
-$boton.addEventListener("mouseleave",manyImages)
+$image.addEventListener('click', manyImages);
+$boton.addEventListener('click', manyImages);
