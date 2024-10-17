ad5daa5c8fe321f2f21234ae8cce121a95a08541
JoanAugusto <pauteromeroj@gmail.com>
2024-10-17 10:14:52 +0200

    commit Cambio en Ejercicio Seguindo daw elimino else

--- Apuntes JavaScript/Ejericicio 17.10.2024 Segundo Daw/segundoDaw.js
+++ Apuntes JavaScript/Ejericicio 17.10.2024 Segundo Daw/segundoDaw.js
@@ -3,9 +3,9 @@
 function segundoDAW(numero){
    
         if(numero%15===0)return 'Segundo DAW';
-        else if(numero%3===0) return'Segundo';
-        else if (numero%5===0) return 'DAW'; 
-        else return '';
+         if(numero%3===0) return'Segundo';
+         if (numero%5===0) return 'DAW'; 
+         return '';
 }
 
 console.log(segundoDAW(15));
