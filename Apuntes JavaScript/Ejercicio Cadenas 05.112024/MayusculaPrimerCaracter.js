b35364a34e5079f1c3164ce1c64b939cc0d01a73
JoanAugusto <pauteromeroj@gmail.com>
2024-11-21 21:13:26 +0100

    Commit ejercicio de clase

--- Apuntes JavaScript/Ejercicio Cadenas 05.112024/MayusculaPrimerCaracter.js
+++ Apuntes JavaScript/Ejercicio Cadenas 05.112024/MayusculaPrimerCaracter.js
@@ -2,11 +2,11 @@
 
 function ucFirst(str){
     let getFirstChar=str[0];//Recordar que un String es como un array podemos acceder mediante por posicion.
-    ;
-}
-ucFirst("doggy");let parser = getFirstChar.toUpperCase() + str.slice (1);
+    let parser = getFirstChar.toUpperCase() + str.slice (1);
 
     let getFirstChar2=str[0].toUpperCase()+str.slice(1);// mas rapido sin en un sola linea de codigo.s
     
     console.log(parser);
-    console.log(getFirstChar2)
+    console.log(getFirstChar2);
+}
+ucFirst("doggy");
--- Apuntes JavaScript/Tarea 21.11.2024 Arrays/transform.js
+++ Apuntes JavaScript/Tarea 21.11.2024 Arrays/transform.js
@@ -1,18 +1,45 @@
 'use strict'
 
 function camelize(str){
-     let text="";
-    let arr= str.split('-');
+   let arr=str.split("-");
+//    console.log(arr);
+//    let coso= String(arr[1]);*
+//    let demas=coso.slice(1);
+//    let letra =coso.slice(0,1).toUpperCase();
+//    console.log(demas);
+//    console.log(arr);
+//    console.log(letra)
   
-    
-    for (let word of arr){ //esto del array arr
-        text += word    
-    }
-    console.log(text);
-    
-    let upper=text.slice(10);
-    
-    console.log(upper);
-    
+   
+//    let junte=letra+demas;
+//    arr[1]=junte;
+//    console.log(arr);
+//    console.log(junte);
+  
+   for(let i=0;i<=arr.length;i++){
+        let text="";
+        
+        if(i+1 <arr.length){// VERIFICAMOS SI HAY SIGUIENTE ELEMENTO
+            let pos=String(arr[i+1]); //Lo que  hace en vez de inicar todo en el primer elemento qyue mepieze en el siguiente
+            
+            let newStr=pos.slice(0,1).toUpperCase()+pos.slice(1);//Obtenemos la primera letra (trasnormado mayus) y obtenemos los demas y lo unimos
+            // console.log(newStr);
+            arr[i+1]=newStr;//sustituiimos el valor original de arr y ponemos el nuevo
+           let newArr=String(arr);
+          
+           let arr2=newArr.split(',');//new array
+           for(let newWords of arr2 ){//valores del array2
+                text += newWords
+           }
+        }else{
+            console.error("Error");
+            
+        }
+        console.log(text);
+       
+       
+        
+   }
 }
-camelize('background-color');
+camelize('background-color-pepe-jeico');
+
