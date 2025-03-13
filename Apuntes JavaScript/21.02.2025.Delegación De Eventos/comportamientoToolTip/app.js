21d99956f6ce196fe76c1158f14750b3f0048e8d
JoanAugusto <pauteromeroj@gmail.com>
2025-03-13 18:18:56 +0100

    commit fin de tooltip

--- Apuntes JavaScript/21.02.2025.Delegación De Eventos/comportamientoToolTip/app.js
+++ Apuntes JavaScript/21.02.2025.Delegación De Eventos/comportamientoToolTip/app.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const $buttonShort=document.querySelectorAll('button');
 const $div=document.getElementById('content');
 
@@ -6,27 +7,36 @@ const $div=document.getElementById('content');
 $buttonShort.forEach(function($button){
     $button.addEventListener('mouseover',(evento)=>{
         
-        
-        let beforeButton=$div.firstElementChild;
+        let beforeButton=$div.firstElementChild.nextElementSibling;
+       
         const newDiv=document.createElement('div');
         let valueButton=evento.target.dataset.tooltip;
         // newDiv.appendChild(valueButton);
        
         
         // console.dir(evento.target.dataset.tooltip);
-        beforeButton.appendChild(newDiv,beforeButton);
-        newDiv.outerHTML=`<p id=tool>${valueButton}</p>`;
-
-        // console.dir($buttonShort);
+        beforeButton.before(newDiv);
+        newDiv.outerHTML=`<div id=tool> <p> ${valueButton}</p> </div>`;
+        
+        
+       
+        console.dir(beforeButton);
+        const $CaptureNewDiv=document.getElementById('tool');
+        $CaptureNewDiv.style=" position:absolute; border:1px solid black; border-radius:1rem; ";
+        if($button.innerHTML==="One more button"){
+         
+                $CaptureNewDiv.style="margin-left:25%; position:absolute; border:1px solid black; border-radius:1rem;" ;
+          
+        }
         
 
 
     });
     $button.addEventListener('mouseout',(evento)=>{
-           
+      
             const $newDiv=document.getElementById('content');
-            console.dir($newDiv.lastElementChild.previousElementSibling.previousElementSibling.lastChild);
-           const $pTool=$newDiv.lastElementChild.previousElementSibling.previousElementSibling.lastChild;
+            console.dir($newDiv.lastElementChild.previousElementSibling.previousElementSibling);
+           const $pTool=$newDiv.lastElementChild.previousElementSibling.previousElementSibling;
            if($newDiv.contains($pTool)){
                 $pTool.remove();
            }
